3f022dc281d8eb0710ccdbb145ed0bb594eb5ffb
Paweł Recław <pawel.reclaw@gmail.com>
2022-12-04 12:08:35 +0100

    Pierwszy commit

---
+++ src/main/java/pl/sda/j133/hibernate/fakturownia/Main.java
@@ -0,0 +1,12 @@
+package pl.sda.j133.hibernate.fakturownia;
+
+/**
+ * @author Paweł Recław, AmeN
+ * @project j133-hibernate-fakturownia
+ * @created 04.12.2022
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
